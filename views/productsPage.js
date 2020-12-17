387c4b9802d1714a761a256c9978012e40d9c12a
Liza <el.dubogryz@gmail.com>
2020-12-17 09:55:43 +0200

    Update productsPage.js

--- views/productsPage.js
+++ views/productsPage.js
@@ -1,60 +1,51 @@
 const view = (products) => `
 <main id="main">
-    ${products.map((product) => `
-    <div class="product-item">
-        <span class="product-name">${product.productName}</span>
-        <span class="product-desc">${product.productDescription}</span>
-    </div>
-    `)}
+    <div class="container">
+    		<div class="products-section">
+	    		<div class="row">
+		    		<div class="col-xl-3 col-lg-4 col-sm-6 col-xs-12">
+		    			<div class="bs-component">
+		    				<div class="card mb-3">
+		    				${products.map((product) => `
+							  <div class="product-image mb-1 mt-1">
+							  	<img src=${product.image} alt=${product.productName}>
+							  	<!-- <div class="dp-product-block__weight">${product.weight}</div> -->
+							  </div>
+							  <div class="card-body">
+							    <h5 class="card-title">${product.productName}</h5>
+							    <h6 class="card-subtitle text-muted">${product.productDescription}</h6>
+							  </div>
+							  <div class="bs-component">
+					              <div class="btn-group btn-group-toggle" data-toggle="buttons">
+					                <label class="btn btn-primary active">
+					                  <input type="radio" name="options" id="option1" autocomplete="off" checked=""> Маленькая
+					                </label>
+					                <label class="btn btn-primary">
+					                  <input type="radio" name="options" id="option2" autocomplete="off"> Средняя
+					                </label>
+					                <label class="btn btn-primary">
+					                  <input type="radio" name="options" id="option3" autocomplete="off"> Большая
+					                </label>
+					              </div>
+					          </div>
+					          <div class="row price-buy-row card-body">
+					          	<div class="price-buy col-6"> 
+					          		<span class="product-price">${product.price}</span>
+					          		<span class="product-currency">грн</span>
+					          	</div>
+					          	<div class="bs-component col-6">
+						          	<button type="button" class="btn btn-primary">В корзину</button>
+						        </div>
+					          </div>
+					        `)}
+							</div>
+		    			</div>
+		    		</div>	
+		        </div>
+	    	</div>
+    	</div>
 </main>
 `;
-// <main id="main">
-//     <div class="container">
-//     		<div class="products-section">
-// 	    		<div class="row">
-// 		    		<div class="col-xl-3 col-lg-4 col-sm-6 col-xs-12">
-// 		    			<div class="bs-component">
-// 		    				<div class="card mb-3">
-// 		    				${products.map((product) => `
-// 							  <div class="product-image mb-1 mt-1">
-// 							  	<img src=${product.image} alt=${product.productName}>
-// 							  	<!-- <div class="dp-product-block__weight">${product.weight}</div> -->
-// 							  </div>
-// 							  <div class="card-body">
-// 							    <h5 class="card-title">${product.productName}</h5>
-// 							    <h6 class="card-subtitle text-muted">${product.productDescription}</h6>
-// 							  </div>
-// 							  <div class="bs-component">
-// 					              <div class="btn-group btn-group-toggle" data-toggle="buttons">
-// 					                <label class="btn btn-primary active">
-// 					                  <input type="radio" name="options" id="option1" autocomplete="off" checked=""> Маленькая
-// 					                </label>
-// 					                <label class="btn btn-primary">
-// 					                  <input type="radio" name="options" id="option2" autocomplete="off"> Средняя
-// 					                </label>
-// 					                <label class="btn btn-primary">
-// 					                  <input type="radio" name="options" id="option3" autocomplete="off"> Большая
-// 					                </label>
-// 					              </div>
-// 					          </div>
-// 					          <div class="row price-buy-row card-body">
-// 					          	<div class="price-buy col-6"> 
-// 					          		<span class="product-price">${product.price}</span>
-// 					          		<span class="product-currency">грн</span>
-// 					          	</div>
-// 					          	<div class="bs-component col-6">
-// 						          	<button type="button" class="btn btn-primary">В корзину</button>
-// 						        </div>
-// 					          </div>
-// 					        `)}
-// 							</div>
-// 		    			</div>
-// 		    		</div>	
-// 		        </div>
-// 	    	</div>
-//     	</div>
-// </main>
-// `;
 
 
 export default view;
