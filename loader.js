6e8cf9b15728b530c49a1df8c1f0d100b00482c1
Liza <el.dubogryz@gmail.com>
2020-12-21 19:48:12 +0200

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,15 +1,11 @@
 import TemplateProcessor from './templateProcessor.js';
-import LoaderPage from './views/loaderPage.js';
+import view from './views/loaderPage.js';
 
 const templateProcessor = new TemplateProcessor();
 
 class Loader{
 	showLoader(){
-		let view;
-		(LoaderPage) =>  {
-	        view = LoaderPage.default;
-	    }
-		templateProcessor.render(view);
+		templateProcessor.render(view.default);
 	}
 }
 
